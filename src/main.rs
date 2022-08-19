2a603519cac8fee4dda9e70f7ec4e6f330e2bf3e
aurazi <jakochrista@gmail.com>
2022-08-19 20:29:46 +0100

    mm new stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 // https://docs.microsoft.com/en-us/windows/win32/learnwin32/your-first-windows-program
+// TODO: make timer thing
+// idk brah
 
 #![allow(non_snake_case)]
 
 use ::std::mem;
-use ::std::ptr::null;
+use ::std::sync::atomic::Ordering;
 use ::std::thread;
+use atomic_float::AtomicF64;
 use crossbeam::channel::{Receiver, Sender};
 use windows::{
     core::{HSTRING, PWSTR},
@@ -12,36 +15,147 @@ use windows::{
     Win32::{
         Foundation::*,
         Graphics::Gdi::*,
-        System::{Environment::GetCommandLineW, LibraryLoader::GetModuleHandleW},
+        System::{Environment::GetCommandLineW, LibraryLoader::GetModuleHandleW, Performance::*},
         UI::WindowsAndMessaging::*,
     },
 };
 
+const FPS: usize = 30;
+
 enum Message {
-    Render,
-    QuitRender,
+    Render(HWND),
+    QuitRender(Sender<()>),
 }
 struct AppState(Sender<Message>);
+struct HighResolutionTimer {
+    t_beginning_of_time: AtomicF64,
+    t_before: AtomicF64,
+    t_after: AtomicF64,
+    c_frequency: f64,
+} // https://docs.microsoft.com/en-us/windows/win32/winmsg/about-timers
+impl HighResolutionTimer {
+    fn new() -> Self {
+        let mut freq = 0;
+        if !unsafe { QueryPerformanceFrequency(&mut freq).as_bool() } {
+            panic!("cry you have no support for HRT");
+        }
+        let mut t_beginning_of_time = 0;
+        unsafe {
+            QueryPerformanceCounter(&mut t_beginning_of_time);
+        }
+        let t_beginning_of_time = t_beginning_of_time as f64;
+        Self {
+            t_beginning_of_time: AtomicF64::new(t_beginning_of_time),
+            t_before: AtomicF64::new(t_beginning_of_time),
+            t_after: AtomicF64::new(t_beginning_of_time),
+            c_frequency: freq as f64,
+        }
+    }
+    fn set_start(&mut self) {
+        unsafe {
+            let mut f = 0;
+            QueryPerformanceCounter(&mut f);
+            self.t_before.store(f as f64, Ordering::Release);
+        }
+    }
+    fn set_end(&mut self) {
+        unsafe {
+            let mut f = 0;
+            QueryPerformanceCounter(&mut f);
+            self.t_after.store(f as f64, Ordering::Release);
+        }
+    }
+    fn get_delta(&self) -> f64 {
+        (self.t_after.load(Ordering::Acquire) - self.t_before.load(Ordering::Acquire))
+            / self.c_frequency
+    }
+    fn get_elapsed(&self) -> f64 {
+        (self.t_after.load(Ordering::Acquire) - self.t_before.load(Ordering::Acquire))
+            / self.c_frequency
+    }
+}
 
 fn BeginListening(recv: Receiver<Message>) {
-    thread::spawn(move || loop {
-        let received_message = recv.recv();
-        match received_message {
-            Ok(message) => match message {
-                Message::Render => {
-                    println!("render request!");
-                }
-                Message::QuitRender => {
-                    println!("goodbye world");
-                    break;
-                }
-            },
-            Err(err) => println!("{err}"),
+    thread::spawn(move || {
+        let mut screen_buffer: Vec<u32> = vec![];
+
+        loop {
+            let received_message = recv.recv();
+            match received_message {
+                Ok(message) => match message {
+                    Message::Render(hwnd) => unsafe {
+                        let mut window_rect = RECT::default();
+                        GetWindowRect(hwnd, &mut window_rect);
+                        let (window_width, window_height) = (
+                            window_rect.right - window_rect.left,
+                            window_rect.bottom - window_rect.top,
+                        );
+                        let buffer_size = window_width * window_height;
+                        if buffer_size as usize > screen_buffer.capacity() {
+                            let cost = buffer_size as usize - (screen_buffer.capacity());
+                            screen_buffer.reserve_exact(cost);
+                            screen_buffer.resize(buffer_size as usize, 0x00000000);
+                        }
+                        // 0x00rrggbb
+                        // do not iterate over entire buffer or you'll be editing parts that
+                        // are not being rendered.
+                        for index in 0i32..buffer_size {
+                            let (px, py) = (index % window_width, index / window_width);
+                            let p = &mut screen_buffer[index as usize];
+                            *p = (px + py) as u32;
+                        }
+
+                        let mut bitmapi = BITMAPINFO::default();
+                        bitmapi.bmiHeader.biSize = mem::size_of::<BITMAPINFOHEADER>() as u32;
+                        bitmapi.bmiHeader.biBitCount = 32;
+                        bitmapi.bmiHeader.biCompression = BI_RGB as u32;
+                        bitmapi.bmiHeader.biPlanes = 1; // has to be 1 idk why
+                        bitmapi.bmiHeader.biWidth = window_width;
+                        bitmapi.bmiHeader.biHeight = window_height;
+
+                        // BeginPaint and EndPaint is weird..
+                        // I get flickers kek~
+                        // anyway, just don't use this
+                        // for WM_PAINT and instead fallback
+                        // to DefWindowProcW
+
+                        let hdc = GetDC(hwnd);
+                        SetStretchBltMode(hdc, COLORONCOLOR);
+                        StretchDIBits(
+                            hdc,
+                            0,
+                            0,
+                            window_width,
+                            window_height,
+                            0,
+                            0,
+                            window_width,
+                            window_height,
+                            screen_buffer.as_ptr() as *const _,
+                            &bitmapi,
+                            DIB_RGB_COLORS,
+                            SRCCOPY,
+                        );
+                        ReleaseDC(hwnd, hdc);
+                    },
+                    Message::QuitRender(sender) => {
+                        println!("goodbye world");
+                        let _ = sender.send(());
+                        break;
+                    }
+                },
+                Err(err) => println!("{err}"),
+            }
         }
     });
 }
 
-fn wWinMain(hInstance: HINSTANCE, hPrevInstance: HINSTANCE, pCmdLine: PWSTR, nCmdShow: i32) -> i32 {
+fn wWinMain(
+    hInstance: HINSTANCE,
+    _hPrevInstance: HINSTANCE,
+    _pCmdLine: PWSTR,
+    nCmdShow: i32,
+) -> i32 {
     const WINDOWCLASSNAME: &HSTRING = w!("Main Window Class");
     const WINDOWNAME: &HSTRING = w!("Hello World Window");
 
@@ -57,8 +171,9 @@ fn wWinMain(hInstance: HINSTANCE, hPrevInstance: HINSTANCE, pCmdLine: PWSTR, nCm
         debug_assert!(regc_r != 0);
     };
 
-    let (sender, receiver) = crossbeam::channel::bounded(30);
-    let appstate = AppState(sender);
+    let (sender, receiver) = crossbeam::channel::bounded(FPS);
+    let mut appstate = AppState(sender);
+    let mut timer = HighResolutionTimer::new();
 
     let hwnd = unsafe {
         CreateWindowExW(
@@ -115,25 +230,28 @@ fn main() {
 }
 
 extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    let appstate: &AppState = unsafe {
+    let appstate: &mut AppState = unsafe {
         if message == WM_CREATE {
             let pCreate = lparam.0 as *const CREATESTRUCTW;
-            let pState = (*pCreate).lpCreateParams as *const AppState;
+            let pState = (*pCreate).lpCreateParams as *mut AppState;
             SetWindowLongPtrW(window, GWLP_USERDATA, pState as isize);
-            &*pState
+            &mut *pState
         } else {
-            &*(GetWindowLongPtrW(window, GWLP_USERDATA) as *const AppState)
+            &mut *(GetWindowLongPtrW(window, GWLP_USERDATA) as *mut AppState)
         }
     };
 
     unsafe {
         match message as u32 {
-            WM_PAINT => {
-                let _ = appstate.0.try_send(Message::Render);
+            WM_EXITSIZEMOVE | WM_SIZING | WM_SHOWWINDOW => {
+                let _ = appstate.0.try_send(Message::Render(window));
                 LRESULT(0)
             }
             WM_DESTROY => {
-                let _ = appstate.0.send(Message::QuitRender);
+                // haha im so cheeky
+                let (sender, reciever) = crossbeam::channel::bounded::<()>(1);
+                let _ = appstate.0.send(Message::QuitRender(sender));
+                let _ = reciever.recv();
                 PostQuitMessage(0); // Push special message to queue, value 0, tells GetMessageW to stop in the next iteration
                 LRESULT(0)
             }
